ff947e5fc6ab322ff1790778df952993cc8c7b16
haseyuy <haseyuy@gmail.com>
2021-06-23 22:17:04 +0900

    Initial Commit

---
+++ TrainingApp/TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppApp.swift
+//  TrainingApp
+//
+//  Created by 長谷侑弥 on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
